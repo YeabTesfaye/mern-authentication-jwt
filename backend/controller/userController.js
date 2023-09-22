f93332f63b4b5bb4660a201765dd19837cd3c390
Yeabsra <tesfayeyeabsra446@gmail.com>
2023-09-22 04:21:41 +0300

    added protect route

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -6,22 +6,21 @@ import { generateToken } from "../utils/generateToken.js";
 // @access Public
 
 export const authUser = asyncHandler(async (req, res) => {
- const {email, password} = req.body;
- const user = await User.findOne({email});
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
- if (user && (await user.matchPasswords(password))) {
-   generateToken(res, user._id);
-   res.status(201).json({
-     _id: user._id,
-     name: user.name,
-     email: user.email,
-   });
- } else {
-   res.status(400);
-   throw new Error(`Invalid email or password`);
- }
+  if (user && (await user.matchPasswords(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error(`Invalid email or password`);
+  }
 });
-
 
 // @desc Register a new user
 // route POST /api/users
@@ -55,13 +54,13 @@ export const registerUSer = asyncHandler(async (req, res) => {
 // route POST /api/users/logout
 // @access Public
 export const logOutUSer = asyncHandler(async (req, res) => {
-   res.cookie('jwt', '', {
-    httpOnly : true,
-    expires : new Date(0)
-   })
-   res.status(200).json({
-    message : "User Loged Out"
-   })
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({
+    message: "User Loged Out",
+  });
 });
 
 // @desc get  user profile
@@ -69,10 +68,10 @@ export const logOutUSer = asyncHandler(async (req, res) => {
 // @access private
 export const getUserProfile = asyncHandler(async (req, res) => {
   const user = {
-    _id : req.user._id,
-    name : req.user.name,
-    email : req.user.email
-  }  
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
   res.status(200).json(user);
 });
 
@@ -81,22 +80,20 @@ export const getUserProfile = asyncHandler(async (req, res) => {
 // @access private
 export const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
-  if(user){
+  if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
-    if(req.body.password){
-        user.password = req.body.password;
+    if (req.body.password) {
+      user.password = req.body.password;
     }
     const updatedUser = await user.save();
     res.status(200).json({
-        _id: updatedUser._id,
-        name : updatedUser.name,
-        email : updatedUser.email
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
     });
-  }
-  else{
+  } else {
     res.status(404);
-    throw new Error('User not Found')
+    throw new Error("User not Found");
   }
-  
 });
